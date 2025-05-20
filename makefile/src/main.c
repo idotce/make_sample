81cc82439ca6daf55ebe2560f8daf6a5d64a4090
youcheng <youcheng@3dvstar.com>
2025-05-20 10:34:33 +0800

    fix android

--- makefile/src/main.c
+++ makefile/src/main.c
@@ -2,6 +2,28 @@
 
 int main(int argc, char* argv[])
 {
-    printf("hello world!\n");
+#if defined(__GNUC__)
+    printf("__GNUC__\r\n");
+#endif
+#if defined(__clang__)
+    printf("__clang__\r\n");
+#endif
+#ifdef __CUDACC__
+    printf("__CUDACC__\n");
+#endif
+#if (defined __ARM_NEON) || (defined __ARM_NEON__)
+    printf("__ARM_NEON\n");
+#endif
+
+#if defined(__aarch64__)
+  #define ARCH_ARM64 1
+#else
+  #define ARCH_ARM64 0
+#endif
+    printf("__aarch64__ = %d\n", ARCH_ARM64);
+
+    #define align ((32+3)/4*4)
+    printf("align = %d\n", align);
+
     return 0;
 }
